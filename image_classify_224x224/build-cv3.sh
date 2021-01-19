b7eabc693217e70ce42b84144e66c996d9ee9850
yan <yan-wyb@foxmail.com>
2021-01-19 15:49:24 +0800

    Add local compile method
    Signed-off-by: yan <yan-wyb@foxmail.com>

--- image_classify_224x224/build-cv3.sh
+++ image_classify_224x224/build-cv3.sh
@@ -2,20 +2,27 @@
 
 set -e
 
-if [ -z "$1" ]; then
-    echo "usage: $0 <linux sdk dir>"
-    exit 1
+if uname -a | grep "x86"; then
+	if [ -z "$1" ]; then
+		echo "usage: $0 <linux sdk dir>"
+		exit 1
+	fi
+
+	export AQROOT=$1
+	export SDK_DIR=$AQROOT/build/sdk
+	export NNSDK_DIR=$AQROOT/build/sdk/nn_sdk//lib/lib64
+	export OPENCV_ROOT=$SDK_DIR/opencv3
+	export CROSS_COMPILE=aarch64-linux-gnu-
+	export TOOLCHAIN=$AQROOT/../../toolchains/gcc-linaro-aarch64-linux-gnu/bin/
+	export LIB_DIR=$TOOLCHAIN/../aarch64-linux-gnu/libc/lib
+else
+	export OPENCV_ROOT=/usr/include/opencv2
+	export NNSDK_DIR=/usr/lib
+	export DDK_DIR=/usr/share/npu/sdk
+	export TOOLCHAIN=
+	export CROSS_COMPILE=
+
 fi
-
-
-export AQROOT=$1
-export SDK_DIR=$AQROOT/build/sdk
-export NNSDK_DIR=$AQROOT/build/sdk/nn_sdk
-export OPENCV_ROOT=$SDK_DIR/opencv3
-export CROSS_COMPILE=aarch64-linux-gnu-
-export TOOLCHAIN=$AQROOT/../../toolchains/gcc-linaro-aarch64-linux-gnu/bin
-export LIB_DIR=$TOOLCHAIN/../aarch64-linux-gnu/libc/lib
-
 
 make -f makefile-cv3.linux
 
